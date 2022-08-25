2b5c393d41abaa652e9615fa01bb06553b8b1176
allenkong221 <allen.kong@uconn.edu>
2022-08-24 23:16:42 -0400

    updated myApp

--- myApp.js
+++ myApp.js
@@ -1,15 +1,24 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
+app.use("/public", express.static(__dirname + "/public"))
 
+require('dotenv').config()
 
-
-
-
-
-
+app.get("/json", function(req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        response = "Hello json".toUpperCase();
+    } else {
+        response = "Hello json";
+    }
+    res.json({"message": response});
+  });
 
 
 
