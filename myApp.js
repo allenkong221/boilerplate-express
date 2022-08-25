10d377a810ece9d3e99e7df31c5741051e77c8f6
allenkong221 <allen.kong@uconn.edu>
2022-08-25 12:09:14 -0400

    updated myApp

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 let express = require('express');
+let bodyParser = require('body-parser')
 let app = express();
 
 console.log("Hello World");
+
+app.use(function middleware(req, res, next) {
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
@@ -20,17 +31,37 @@ app.get("/json", function(req, res) {
     res.json({"message": response});
   });
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({"time": req.time});
+});
 
+app.get("/:word/echo", function(req, res) {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
 
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  // var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
 
-
-
-
-
-
-
-
-
+app.post("/name", function(req, res) {
+  var { first: firstName, last: lastName } = req.body
+  res.json({
+    name: `${firstName} ${lastName}`
+  })
+});
 
 
 
